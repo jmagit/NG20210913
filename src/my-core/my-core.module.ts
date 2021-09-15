4a3cff7f46d1db519fa92110f4351ac096a28bad
Javier <jmartin@grupoloyal.com>
2021-09-15 18:42:23 +0200

    pipes

--- src/my-core/my-core.module.ts
+++ src/my-core/my-core.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PIPES_CADENAS } from './pipes/cadenas.pipe';
+import { SizerComponent } from './components/sizer.component';
 
 
 
 @NgModule({
   declarations: [
-
+    PIPES_CADENAS, SizerComponent,
   ],
   exports: [
-
+    PIPES_CADENAS, SizerComponent,
   ],
   imports: [
     CommonModule
